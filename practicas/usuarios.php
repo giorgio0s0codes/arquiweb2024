a98ab4c615914aa50e660128a48bb97cf0968a92
EC2 Default User <ec2-user@ip-172-31-18-233.us-east-2.compute.internal>
2024-10-03 15:05:55 +0000

    my commit using my terminal

---
+++ practicas/usuarios.php
@@ -0,0 +1,13 @@
+<?php
+    $FH = fopen('../DB/usuarios.csv', 'r');
+?>
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="utf-8">
+        <title>Usuarios</title>
+    </head>
+    <body>
+        <h1>Usuarios</h1>
+    </body>
+</html>
