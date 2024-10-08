5a23aeb7ffd0ddc9d7a42ee58858ec1ebf9472fe
EC2 Default User <ec2-user@ip-172-31-18-233.us-east-2.compute.internal>
2024-10-08 14:18:39 +0000

    This commit includes functions to search in a DB using php.

--- practicas/funciones.php
+++ practicas/funciones.php
@@ -8,21 +8,41 @@ function insertarUsuario($usuario){
 function getUsuarios(){
 	//Retorna arreglo de arreglos asociativos
 	$file_handle = fopen("../DB/usuarios.csv","r");
-	$users = fread($file_handle, filesize('../DB/usuarios.csv'));
-	$lineas = explode("\n",$users);
+	$data = fread($file_handle, filesize('../DB/usuarios.csv'));
+	fclose($file_handle);
+	$lineas = explode("\n",$data);
+	$users = array();
 	foreach($lineas as $linea){
 		$values = explode(",", $linea);
+		if(!count($values)==7){
+			continue;
+		}
 		$user = array("usuario" => $values[0],
 				"nombre" => $values[1],
 				"apellidoP" => $values[2],
-				"apellidoM"
-				"correo"
-				"contraseña"
-				"plan"
+				"apellidoM" => $values[3],
+				"correo" => $values[4],
+				"password" => $values[5],
+				"plan" => $values[6]);
+		array_push($users,$user);
 	}
+	 return $users;
 }
 
-function getUsuario(){
+function getUsuario($usrsrch){
+	$usrsrch = getUsuarios();
+	/*
+    foreach ($usuario as $usarios) {
+        if ($usrsrch== $usuario["usuario"]) {
+            return $user;
+        }
+    }
+	*/
+
+	array_filter($usuarios, function($usuario) use($usrsrch){
+		return $usuario["usuario"] == $usrsrch;
+	});
+	return null;
 
 }
 
@@ -30,6 +50,14 @@ if(isset($_GET["prueba"]) && isset($_GET["method"]) && $_GET["method"] == "getUs
 	//Probar el método getUsuarios:
 	
 	echo "probando getUSuarios";
-	getUsuarios();
+	var_dump(getUsuarios());
+	
+}
+
+if(isset($_GET["prueba"]) && isset($_GET["method"]) && $_GET["usr"] && $_GET["method"] == "getUsuarios"){
+	//Probar el método getUsuarios:
+	
+	echo "probando getUSuario";
+	var_dump(getUsuario($_GET["usr"]));
 	
 }
