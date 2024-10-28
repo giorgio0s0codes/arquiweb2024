48b80cf4e4e8f2e4df6448d74b410bd85445ec6a
EC2 Default User <ec2-user@ip-172-31-18-233.us-east-2.compute.internal>
2024-10-28 14:53:51 +0000

    This contains a new file who uses bootstrap to edit a file.

--- apuntes/CSS/responsivo.php
+++ apuntes/CSS/responsivo.php
@@ -2,10 +2,37 @@
 <html>
     <head>
         <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, inital-scale = 1">
         <title>Diseño Responsivo</title>
-        <link href="./css/grid.css" rel="stylesheet">
+        <link href="./grid.css" rel="stylesheet">
     </head>
     <body>
-
+        <div class="row">
+            <h1>Grid Básico</h1>
+            <div class="col-md-6 col-lg-3">
+                <p>Item 1</p>
+            </div>
+            <div class="col-md-6 col-lg-3">
+                <p>Item 2</p>
+            </div>
+            <div class="col-md-6 col-lg-3">
+                <p>Item 3</p>
+            </div>
+            <div class="col-md-6 col-lg-3">
+                <p>Item 4</p>
+            </div>
+            <div class="col-md-6 col-lg-3">
+                <p>Item 5</p>
+            </div>
+            <div class="col-md-6 col-lg-3">
+                <p>Item 6</p>
+            </div>
+            <div class="col-md-6 col-lg-3">
+                <p>Item 7</p>
+            </div>
+            <div class="col-md-6 col-lg-3">
+                <p>Item 8</p>
+            </div>
+        </div>
     </body>
 </html>
