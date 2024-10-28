e5d0c5fefc3d6c906f1176f88fc55d53dea1f25f
EC2 Default User <ec2-user@ip-172-31-18-233.us-east-2.compute.internal>
2024-10-28 13:41:20 +0000

    This contains a new file who is responsive to different screens accesing the site.

---
+++ apuntes/CSS/responsivo.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Diseño Responsivo</title>
+        <link href="./css/grid.css" rel="stylesheet">
+    </head>
+    <body>
+
+    </body>
+</html>
