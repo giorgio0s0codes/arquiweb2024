e87eca60aa2033c215444eccd21d30eb4093653d
EC2 Default User <ec2-user@ip-172-31-18-233.us-east-2.compute.internal>
2024-11-14 14:33:03 +0000

    This new todo2, is the structure base to connect to a db.

--- apuntes/javascript/js/todo2.js
+++ apuntes/javascript/js/todo2.js
@@ -14,6 +14,7 @@ var tareasData = {
         this.tareas.forEach(function(tarea,index){
             if(tarea.idTarea == idTarea){
                 (this.tareas[index]).finalizada = true;
+                return this.tareas[index];
             }
         }, this);
 
@@ -29,3 +30,57 @@ var tareasData = {
         });
     },
 };
+
+var btnAgregar = document.getElementById("addTarea");
+
+btnAgregar.addEventListener("click", function(e){
+    var textoTarea = document.getElementById("tarea").value;
+
+    tareasData.addTarea(textoTarea);
+
+    var tbd_pend = document.getElementById("tareasPendientes");
+    tbd_pend.innerHTML = "";
+
+    tbd_pend.innerHTML = tareasData.tareas.map(function(tarea){
+
+        return `<tr id="tr_${tarea.idTarea}">
+                    <td id="nombreTarea_${tarea.idTarea}">${tarea.tarea}</td>
+                    <td>
+                        <input> id "chk_${tarea.idTarea}"
+                                type = "checkbox"
+                                name = "chktarea"
+                                class = "ctrTarea"
+                                data-idTarea = "${tarea.tarea}">
+                        </td>
+                    </tr>`;
+    }).join('');
+    
+    document.getElementById("tarea").value = "";
+    document.getElementById("tarea").focus();
+    eventosEliminarTarea();
+
+});
+
+function eventosEliminarTarea(){
+
+    chcbxs = document.querySelectorAll("input.ctrTarea");
+    Array.from(chcbxs).forEach(function(chk){
+
+        chk.addEventListener("click",function(event){
+
+            var idTarea = this.getAttribute("data-idTarea");
+            var trfnz = tareasData.finalizarTarea(idTarea);
+
+            var tbyFinal = document.getElementById("t_realizada");
+
+            tbyFinal.innerHTML = "";
+
+            tbyFinal.innerHTML = tareasData.getFinalizadas.map(function(tarea){
+                return `<tr class = "table-info">
+                            <td>${tarea.tarea}</td>
+                        </tr>`;
+            }).join('');
+
+        });
+    });
+}
