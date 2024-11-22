60d10d895afce37c532930a34ab506d8fe67ce51
EC2 Default User <ec2-user@ip-172-31-18-233.us-east-2.compute.internal>
2024-11-22 23:07:47 +0000

    This is the config file for the php to connect to the DB.

---
+++ pruebaTec/config/configBakeDB.php
@@ -0,0 +1,27 @@
+<?php
+
+/*DSN Data Source Name <manejador>:host=<ip o nombre host>;dbname=<base de datos>;
+                                   charset=<charset>*/
+
+$dsn		= "mysql:host=localhost;dbname=productosBake;charset=utf8mb4";
+
+$usuario 	= "arqwebo2024";
+$password	= "210681";
+
+$options	= array(
+				PDO::ATTR_ERRMODE				=> PDO::ERRMODE_EXCEPTION,
+				PDO::ATTR_DEFAULT_FETCH_MODE	=> PDO::FETCH_ASSOC,
+				PDO::ATTR_EMULATE_PREPARES		=> true 
+			  );
+
+
+try{
+
+	$CNX = new PDO($dsn, $usuario, $password, $options);
+
+}catch(Exception $e){
+
+	echo "Error de conexión a la base de datos:";
+	echo $e->getMessage();
+	exit();
+}
